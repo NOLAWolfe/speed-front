3a5eaf4ae31924ff0b415b85bb9860b055192385
Alex Wolfe <troy.hicks1@yahoo.com>
2023-07-11 15:06:35 -0500

    Nav Bar Responsive Changes

--- src/Component/NavBar/NavBar.js
+++ src/Component/NavBar/NavBar.js
@@ -21,14 +21,17 @@ class NavBar extends Component {
             <div>
                 <Grid container className='navbar__main'>
                     {/* totall summation value of grid should be 12 */}
-                    <Grid item xs = {3}>
+{/* r xs 6 sm 6 md 3 lg 3*/}
+{/* m xs hide, md hide, md 6, lg 6 */}
+{/* l xs 6 sm 6 md 3 lg 3*/}
+                    <Grid item xs = {6} sm={6} md={3} lg={3}>
                         <div className="navbar__leftBar">
                             <img className='navbar__logo' src={spdlogo} width="40px " alt=''/>
                             <input className="navbar__search" type="text" placeholder='Search The Speed'></input>
                         </div>
                     </Grid>
 
-                    <Grid item xs = {6}>
+                    <Grid item sx={{display: {xs: 'none', sm: 'none', md:'block', lg:'block'}}}md={6} lg={6}>
                         <div className="navbar__container">
                             <div className="navbar__tabs active">
                                 <img src={home} height="35px" width="35px" alt="" />
@@ -48,7 +51,7 @@ class NavBar extends Component {
                         </div>
                     </Grid>
 
-                    <Grid item xs = {3}>
+                    <Grid item xs={6} sm={6} md={3} lg={3}>
                         <div className="navbar__right">
                             <div className="navbar__righttab">
                                 <Avatar className='navbar__rightimg' src="https://scontent.ffcm1-2.fna.fbcdn.net/v/t39.30808-1/327561013_582471203324506_3040817152926035477_n.jpg?stp=cp0_dst-jpg_p80x80&_nc_cat=106&cb=99be929b-3346023f&ccb=1-7&_nc_sid=7206a8&_nc_ohc=mgx9rFCWT20AX_NdQpW&_nc_oc=AQlzm2E-z59BfMjPIMadS1Pipw49f-uoqjp21g4kq0pd3T6fC4QlUz4W2DPrKSGmUhI&_nc_ht=scontent.ffcm1-2.fna&oh=00_AfCltIWW_jBRVCRvxeTENd5e2BF4uS3nQ7F0q15AjsTJ0g&oe=64AE065A"/>
